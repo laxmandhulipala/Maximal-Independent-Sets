a00daca29776eaff0739ad3750fad043845f4e57
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2016-02-17 05:23:05 -0500

    fix luby

--- apps/Luby.C
+++ apps/Luby.C
@@ -1,54 +1,4 @@
 #include "ligra.h"
-#include "math.h"
-#include <stdlib.h> 
-
-// seq version of luby
-template <class vertex>
-int runLubyIteration(graph<vertex>& G, bool* inMis, bool* active, int* priorities) {
-  const intE n = G.n;
-
-  // all active v pick a priority
-  // non-det: det version uses sequential for
-  {parallel_for(long i=0;i<n;i++) if (active[i]) priorities[i] = rand();}
-//  for (int i = 0; i < n; i++) {
-//    if (active[i]) {
-//      priorities[i] = rand();
-//    }
-//  }
-
-  // loop over active v. Check if ngh is in MIS or 
-  {parallel_for (int i = 0; i < n; i++) {
-    if (active[i]) {
-      intE outDeg = G.V[i].getOutDegree();
-      bool stillIn = true;
-      for (int j = 0; j < outDeg; j++) {
-        intE ngh = G.V[i].getOutNeighbor(j);
-        if (inMis[ngh]) {
-          active[i] = false;
-          inMis[i] = false;
-          stillIn = false;
-          break;
-        }
-        if (active[ngh] && priorities[ngh] >= priorities[i]) {
-          stillIn = false;
-        }
-      }
-      if (stillIn) {
-        inMis[i] = true;
-        active[i] = false;
-      }
-    }
-  }}
-
-  int numActive = 0;
-  for (int i = 0; i < n; i++) {
-    if (active[i]) {
-      numActive++;
-    }
-  }
-
-  return numActive;
-}
 
 template<class vertex>
 bool checkMis(graph<vertex>& G, bool* inMis) {
@@ -59,7 +9,6 @@ bool checkMis(graph<vertex>& G, bool* inMis) {
     int numConflict = 0;
     bool iInMis = inMis[i];
     for (int j = 0; j < outDeg; j++) {
-      // really should write iterator but v lazy
       intE ngh = G.V[i].getOutNeighbor(j);
       if (iInMis && inMis[ngh]) {
         numConflict++;
@@ -73,42 +22,101 @@ bool checkMis(graph<vertex>& G, bool* inMis) {
   return correct;
 }
 
-// ignore using functional-primitives just to make MIS 
-// computation as explicit as possible
+struct Luby_P1 {
+  int* priorities;
+  bool* active;
+  bool* inMis;
+  Luby_P1(int* _priorities, bool* _active, bool* _inMis) : 
+    priorities(_priorities), active(_active), inMis(_inMis) {}
+  inline bool update (uintE s, uintE d) { 
+    if (priorities[s] < priorities[d]) {
+      inMis[s] = false;
+    }
+    return 0; 
+  }
+  inline bool updateAtomic (uintE s, uintE d) {
+    if (priorities[s] > priorities[d]) {
+      (CAS(&inMis[s],true,false));
+    }
+    return 0;
+  }
+  inline bool cond (uintE d) { return (active[d] == true); } 
+};
+
+struct Luby_P2 {
+  bool* active;
+  bool* inMis;
+  Luby_P2(bool* _active, bool* _inMis) : 
+    active(_active), inMis(_inMis) {}
+  inline bool update (uintE s, uintE d) {
+    active[s] = false;
+    return 0;
+  }
+  inline bool updateAtomic (uintE s, uintE d) { 
+    (CAS(&active[s], true, false));
+    return 0;
+  }
+  inline bool cond (uintE d) { return (inMis[d] == true); } 
+};
+
+struct Luby_Vertex_F {
+  int* priorities;
+  bool* inMis;
+  Luby_Vertex_F(int* _priorities, bool* _inMis) : 
+    priorities(_priorities), inMis(_inMis) {} 
+  inline bool operator ()  (uintE i) {
+    priorities[i] = rand(); 
+    inMis[i] = true;
+    return 1;
+  }
+};
+
+struct Luby_Filter {
+  bool* active;
+  bool* inMis;
+  Luby_Filter(bool* _active, bool* _inMis) : 
+    active(_active), inMis(_inMis)  {}
+  inline bool operator () (uintE i) {
+    return (active[i] && !inMis[i]);
+  }
+};
+
 template <class vertex>
-void Compute(graph<vertex>& G, commandLine P) {
-  srand(4);
-  const intE n = G.n;
+void Compute(graph<vertex>& GA, commandLine P) {
+  const intE n = GA.n;
+  bool checkCorrectness = P.getOptionValue("-checkCorrectness");
 
   bool* inMis = newA(bool, n);
   bool* active = newA(bool, n);
+  bool* frontier_data = newA(bool, n);
   int* priorities = newA(int, n);
   {parallel_for(long i=0;i<n;i++) {
-      inMis[i] = 0;
-      active[i] = 1;
+    inMis[i] = 0;
+    active[i] = 1;
+    frontier_data[i] = 1;
   }}
 
-  int numActive = n;
-
   int round = 0;
-  while (numActive > 0) {
-    printf("round = %d, numActive = %d\n", round, numActive);
-    numActive = runLubyIteration(G, inMis, active, priorities);
+  vertexSubset Frontier(n, frontier_data);
+  while (!Frontier.isEmpty()) {
+    // assign priorities. set all active vertices to be in MIS
+    vertexMap(Frontier, Luby_Vertex_F(priorities, inMis));
+    // compare priorities. unset from MIS if p(u) > p(v) for any (u,v)
+    edgeMap(GA, Frontier, Luby_P1(priorities, active, inMis), GA.m/20);
+    // make vertex inactive if for any (u,v) v \in MIS
+    edgeMap(GA, Frontier, Luby_P2(active, inMis), GA.m/20);
+    vertexSubset output = vertexFilter(Frontier, Luby_Filter(active, inMis));
+    Frontier.del();
+    Frontier = output;
     round++;
   }
-  printf("Finished after %d rounds\n", round);
 
-  int numInMis = 0;
-  for (int i = 0; i < n; i++) {
-    if (inMis[i]) {
-      numInMis++;
-    }
+  if (checkCorrectness) {
+    cout << "Mis correctness : " << checkMis(GA, inMis) << endl;
   }
-  bool correct = checkMis(G, inMis);
-  printf("numInMis = %d\n", numInMis);
-  printf("correct = %d\n", correct);
 
   free(inMis);
   free(active);
   free(priorities);
+  Frontier.del();
 }
